c17e83935cb827f1085c77e003944322b752ba02
MuhamadAndre10 <andrepriyanto95@gmail.com>
2024-02-05 13:55:17 +0700

    adding redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,29 @@
 const { createStore } = require("redux");
 const { createSlice } = require("@reduxjs/toolkit");
 
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    // state is the current state, automatically passed by Redux
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    // action is the payload, automatically passed by Redux
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
